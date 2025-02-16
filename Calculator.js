2cf091f3a6c174959ed29acc30fcd276c69f5447
Avishek <gupavishek00000@gmail.com>
2025-02-16 22:18:24 +0545

    Done !!

---
+++ Calculator.js
@@ -0,0 +1,24 @@
+let calculator_input_Number_V = document.getElementById(
+  "calculator_input_Number"
+);
+let calculator_input_Number_Previews_V = document.getElementById(
+  "calculator_input_Number_Previews"
+);
+let Calculator_input_C_V = document.getElementById("Calculator_input_C");
+let DisplayValue;
+
+function display(DisplayValue) {
+  calculator_input_Number_V.value += DisplayValue;
+}
+
+Calculator_input_C_V.addEventListener("click", () => {
+  calculator_input_Number_V.value = "";
+  calculator_input_Number_Previews_V.value = "";
+});
+
+function displaySolve() {
+  let eq = calculator_input_Number_V.value;
+  calculator_input_Number_Previews_V.value = eq;
+  let y = eval(eq);
+  calculator_input_Number_V.value = y;
+}
